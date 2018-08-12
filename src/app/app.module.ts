ade09dfffbaabb5c83556f1d29a1cc0ead79aabe
VanausloosThomas <thomas.vanausloos@cegeka.com>
2018-08-12 08:27:14 +0200

    Thomas: first version of save-the-date cards made with angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {MatCardModule, MatIconModule} from '@angular/material';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatCardModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
